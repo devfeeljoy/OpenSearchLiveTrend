ed6f085ab5afaf915d3c9bc066cb0e4115fc0516
woopil <jwp4465@highdev.pro>
2024-01-02 14:10:34 +0900

    fix: opensearch live_trends

--- hello-world/main.go
+++ hello-world/main.go
@@ -15,7 +15,6 @@ import (
 	"github.com/linkedin/goavro/v2"
 	"net/http"
 	"os"
-	"strconv"
 )
 
 func HandleRequest(ctx context.Context, s3Event events.S3Event) {
@@ -31,8 +30,6 @@ func HandleRequest(ctx context.Context, s3Event events.S3Event) {
 
 		bucket := record.S3.Bucket.Name
 		key := record.S3.Object.Key
-		//bucket := "burige.314257712735.ap-northeast-2"
-		//key := "NewRank/2023-12-01/buridge_dy_account_daily_data_20231201.avro"
 
 		// S3에서 Avro 파일 가져오기
 		result, err := s3Client.GetObject(&s3.GetObjectInput{
@@ -85,64 +82,10 @@ func HandleRequest(ctx context.Context, s3Event events.S3Event) {
 				}
 			}
 
-			// "totalSales" 필드를 숫자로 변환
-			totalSalesStr, ok := rawDatum["totalSales"].(string)
-			if ok {
-				totalSales, err := strconv.ParseFloat(totalSalesStr, 64)
-				if err == nil {
-					rawDatum["totalSales"] = totalSales
-				}
-			}
-
-			// "maxPrice" 필드를 숫자로 변환
-			maxPriceStr, ok := rawDatum["maxPrice"].(string)
-			if ok {
-				maxPrice, err := strconv.ParseFloat(maxPriceStr, 64)
-				if err == nil {
-					rawDatum["maxPrice"] = maxPrice
-				}
-			}
-
-			// "fansIncRate" 필드를 숫자로 변환
-			fansIncRateStr, ok := rawDatum["fansIncRate"].(string)
-			if ok {
-				fansIncRate, err := strconv.ParseFloat(fansIncRateStr, 64)
-				if err == nil {
-					rawDatum["fansIncRate"] = fansIncRate
-				}
-			}
-
-			// "avgViewDuration" 필드를 숫자로 변환
-			avgViewDurationStr, ok := rawDatum["avgViewDuration"].(string)
-			if ok {
-				avgViewDuration, err := strconv.ParseFloat(avgViewDurationStr, 64)
-				if err == nil {
-					rawDatum["avgViewDuration"] = avgViewDuration
-				}
-			}
-
-			// "avgSalePrice" 필드를 숫자로 변환
-			avgSalePriceStr, ok := rawDatum["avgSalePrice"].(string)
-			if ok {
-				avgSalePrice, err := strconv.ParseFloat(avgSalePriceStr, 64)
-				if err == nil {
-					rawDatum["avgSalePrice"] = avgSalePrice
-				}
-			}
-
-			// "salesTransRate" 필드를 숫자로 변환
-			salesTransRateStr, ok := rawDatum["salesTransRate"].(string)
-			if ok {
-				salesTransRate, err := strconv.ParseFloat(salesTransRateStr, 64)
-				if err == nil {
-					rawDatum["salesTransRate"] = salesTransRate
-				}
-			}
-
 			batchData = append(batchData, rawDatum)
 
 			// 배치 크기에 도달하거나 마지막 레코드인 경우 색인화
-			if len(batchData) >= 1000 {
+			if len(batchData) >= 100 {
 				err = indexBatchToOpenSearch(batchData, openSearchURL)
 				if err != nil {
 					fmt.Printf("Error indexing batch to OpenSearch: %s\n", err)
@@ -170,7 +113,7 @@ func indexBatchToOpenSearch(batchData []interface{}, openSearchURL string) error
 	var buffer bytes.Buffer
 	for _, data := range batchData {
 		metaData := map[string]map[string]string{
-			"index": {"_index": "lives"},
+			"index": {"_index": "live_trends"},
 		}
 		jsonMeta, _ := json.Marshal(metaData)
 		buffer.Write(jsonMeta)
